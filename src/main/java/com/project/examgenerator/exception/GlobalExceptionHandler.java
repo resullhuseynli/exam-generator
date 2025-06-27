c99a240f8710065965306d9e915f8ed639216cc1
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-28 00:02:38 +0400

    GlobalExceptionHandler handle FileNotFoundException

--- src/main/java/com/project/examgenerator/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/examgenerator/exception/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.project.examgenerator.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.io.FileNotFoundException;
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(FileNotFoundException.class)
+    public ResponseEntity<ApiResponse<String>> handleFileNotFoundException(FileNotFoundException fileNotFoundException) {
+        return ResponseEntity.badRequest().body(new ApiResponse<>(fileNotFoundException.getMessage(), LocalDateTime.now()));
+    }
+
 }
