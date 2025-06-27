0abbfedb322b58ca1989f133d0552ba58c5cf4ac
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-27 20:03:04 +0400

    Git Initialized!

---
+++ src/test/java/com/project/examgenerator/ExamGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.examgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
