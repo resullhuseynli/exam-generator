ebccfb727acb083202441d1ca55db5759db45e16
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-29 14:09:35 +0400

    Return type changed

--- src/main/java/com/project/examgenerator/service/ExamService.java
+++ src/main/java/com/project/examgenerator/service/ExamService.java
@@ -1,5 +1,6 @@
 package com.project.examgenerator.service;
 
+import com.project.examgenerator.model.Question;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedHashMap;
+import java.util.*;
 
 @Service
 public class ExamService {
@@ -18,36 +19,45 @@ public class ExamService {
         QUESTION_LINE_SIZE = questionLineSize;
     }
 
-    public LinkedHashMap<String, Integer> getQuestions(String filename,
-                                                       int startPoint,
-                                                       int endPoint,
-                                                       int questionCount) throws IOException {
-        LinkedHashMap<String, Integer> questions = new LinkedHashMap<>();
+    public List<Question> getQuestions(String filename,
+                                                                  int startPoint,
+                                                                  int endPoint,
+                                                                  int questionCount) throws IOException {
+        List<Question> questions = new ArrayList<>();
+        final long LINE_COUNT = countLines(filename, endPoint);
         try (BufferedReader br = new BufferedReader(new FileReader("uploads/" + filename))) {
+            for (int i = 1; i < startPoint; i++) {
+                for (int j=0 ; j < QUESTION_LINE_SIZE; j++) {
+                    br.readLine();
+                }
+            }
             String line = br.readLine();
-            while (line != null) {
-                int trueAnswer = 0;
+            int range = endPoint - startPoint;
+            int questionNumber = 0;
+            while (line != null && questionNumber != endPoint) {
+                Question questionGenerated = new Question();
+                ArrayList<String> answers = new ArrayList<>();
                 int counter = 0;
-                StringBuilder question = new StringBuilder();
                 while (counter < QUESTION_LINE_SIZE) {
                     if (counter == 0) {
+                        questionGenerated.setQuestionNumber(getQuestionNumber(line));
                         line = removeNumber(line);
+                        questionGenerated.setQuestionText(line);
                     } else {
                         if (checkTrue(line)) {
-                            trueAnswer = counter;
+                            questionGenerated.setTrueAnswer(trueVariant(counter));
                         }
                         line = makeVariant(line, counter);
+                        answers.add(line);
                     }
                     counter++;
-                    question.append(line).append('\n');
                     line = br.readLine();
                 }
-                questions.put(question.toString().trim(), trueAnswer);
+                questionGenerated.setAnswers(answers);
+                questions.add(questionGenerated);
             }
         } catch (FileNotFoundException fileNotFoundException) {
-            throw new FileNotFoundException("File not found");
-        }  catch (IOException ioException) {
-            throw new IOException();
+            throw new FileNotFoundException(filename + " not found");
         }
         return questions;
     }
@@ -62,6 +72,18 @@ public class ExamService {
             counter++;
         }
         return question;
+    }
+
+    private char trueVariant(int variant) {
+        char trueVariant = ' ';
+        switch (variant) {
+            case 1 -> trueVariant = 'A';
+            case 2 -> trueVariant = 'B';
+            case 3 -> trueVariant = 'C';
+            case 4 -> trueVariant = 'D';
+            case 5 -> trueVariant = 'E';
+        }
+        return trueVariant;
     }
 
     private boolean checkTrue(String answer) {
@@ -79,6 +101,40 @@ public class ExamService {
             case 5 -> line = "E:" + line;
         }
         return line;
+    }
+
+    private long countLines(String filename, int endPoint) throws IOException {
+        long lineCount = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader("uploads/" + filename))) {
+            while(br.readLine() != null) {
+                lineCount++;
+            }
+            if((lineCount / QUESTION_LINE_SIZE) < endPoint) {
+                throw  new IOException("End point is more than " + lineCount/6);
+            }
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException(filename + " not found");
+        }
+        return lineCount;
+    }
+
+    private int getQuestionNumber(String question) {
+        int counter = 0;
+        while (question.charAt(counter) != '.') {
+            counter++;
+        }
+        return  Integer.parseInt(question.substring(0, counter));
+    }
+
+    private Set<Integer> randomNumbersGenerator(int startPoint, int endPoint, int questionCount) {
+        Set<Integer> randomNumbers = new HashSet<>();
+        int randomNumber;
+        Random random = new Random();
+        while (randomNumbers.size() < questionCount) {
+            randomNumber = random.nextInt(endPoint - startPoint + 1) + startPoint;
+            randomNumbers.add(randomNumber);
+        }
+        return randomNumbers;
     }
 
 }
