b7b72f33e929688c5715a1b1bfb232c32f66869b
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-30 16:46:19 +0400

    allowed methods from frontend

--- back-end/src/main/java/com/project/examgenerator/controller/ExamController.java
+++ back-end/src/main/java/com/project/examgenerator/controller/ExamController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin("*")
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/exam")
 @RequiredArgsConstructor
